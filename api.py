d3c1c90174a2c262280d2a23717c86124c48f7a9
Thiro <tymofyeyeva@free.fr>
2026-03-14 13:37:59 +0100

    add homepage

--- api.py
+++ api.py
@@ -215,3 +215,7 @@ def explorer(request: Request):
         """
 
     return HTMLResponse(html)
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
