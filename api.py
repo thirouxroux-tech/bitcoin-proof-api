183aa93e4e7ae5ca835dd56266689d4581ea7233
Thiro <tymofyeyeva@free.fr>
2026-04-24 16:48:30 +0200

    test version

--- api.py
+++ api.py
@@ -9,11 +9,13 @@ def home():
 
 @app.get("/pay")
 def pay():
+    amount_btc = 0.0001
+
     return {
-        "btc_address": "bc1qtest123",
-        "api_key": "sk_test_123",
-        "amount_btc": 0.0001
-    }
+        "btc_address": "bc1qxxxxxxxxxxxxxxxxxxxxx",  # TON adresse
+        "amount_btc": amount_btc,
+        "currency": "BTC"
+    }    }
 
 @app.get("/app", response_class=HTMLResponse)
 def app_page():
@@ -24,14 +26,16 @@ def app_page():
         <button onclick="pay()">Unlock</button>
         <p id="result"></p>
 
-        <script>
-        async function pay(){
-            let r = await fetch('/pay');
-            let d = await r.json();
-            document.getElementById('result').innerHTML =
-                "Key: " + d.api_key + "<br>BTC: " + d.amount_btc;
-        }
-        </script>
+      <script>
+async function pay(){
+    let r = await fetch('/pay');
+    let d = await r.json();
+
+    document.getElementById('result').innerHTML =
+        "Adresse BTC:<br>" + d.btc_address +
+        "<br><br>Montant: " + d.amount_btc + " BTC";
+}
+</script>
     </body>
     </html>
     """
